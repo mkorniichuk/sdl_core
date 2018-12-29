f603513c4a8e2c730c45693d668d156dadb8669c
Mykola Korniichuk (GitHub) <mkorniichuk@luxoft.com>
2018-12-29 18:06:01 +0200

    Third party cmake
    * CMakeLists adjusted for new structure
    * bash code moved to log4cxx helpers
    * minor changes in find_paths

--- src/3rd_party/bson/QNX.cmake
+++ src/3rd_party/bson/QNX.cmake
@@ -28,7 +28,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-  set(BSON_CONFIGURE_FLAGS
+set(BSON_CONFIGURE_FLAGS
     "--host=${CMAKE_SYSTEM_PROCESSOR}-nto-qnx"
     "--target=qnxnto"
     "--bindir=${QNX_HOST}/usr/bin/"
@@ -44,4 +44,34 @@
     "RANLIB=${QNX_HOST}/usr/bin/nto${CMAKE_SYSTEM_PROCESSOR}-ranlib${HOST_EXECUTABLE_SUFFIX}"
     "AR=${QNX_HOST}/usr/bin/nto${CMAKE_SYSTEM_PROCESSOR}-ar${HOST_EXECUTABLE_SUFFIX}"
     "CXX=${QNX_HOST}/usr/bin/nto${CMAKE_SYSTEM_PROCESSOR}-g++${HOST_EXECUTABLE_SUFFIX}"
-    "CC=${QNX_HOST}/usr/bin/nto${CMAKE_SYSTEM_PROCESSOR}-gcc${HOST_EXECUTABLE_SUFFIX}")  
+    "CC=${QNX_HOST}/usr/bin/nto${CMAKE_SYSTEM_PROCESSOR}-gcc${HOST_EXECUTABLE_SUFFIX}"
+)
+
+set(CONFIGURE_COMMAND
+  touch aclocal.m4 configure.ac Makefile.am Makefile.in configure config.h.in && ./configure ${CONFIGURE_FLAGS})
+
+
+ExternalProject_Add(
+  libbson
+  GIT_REPOSITORY "http://github.com/smartdevicelink/bson_c_lib.git"
+  GIT_TAG "master"
+  DOWNLOAD_DIR ${BSON_LIB_SOURCE_DIRECTORY}
+  SOURCE_DIR ${BSON_LIB_SOURCE_DIRECTORY}
+  CONFIGURE_COMMAND ${CONFIGURE_COMMAND}
+  BUILD_IN_SOURCE true
+  INSTALL_COMMAND ""
+  UPDATE_COMMAND ""
+)
+
+set(INSTALL_COMMAND "make install")
+
+if (${3RD_PARTY_INSTALL_PREFIX} MATCHES "/usr/local")
+  set(INSTALL_COMMAND "sudo ${INSTALL_COMMAND}")
+endif()
+
+install(
+  CODE "execute_process(
+    WORKING_DIRECTORY ${BSON_LIB_SOURCE_DIRECTORY}
+    COMMAND /bin/bash -c \"${INSTALL_COMMAND}\"
+  )"
+)
